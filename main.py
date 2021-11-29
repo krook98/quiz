17db22374c9296cd51e53d68b303682cdf6f0d4e
sokal <sokalski.jakub98@gmail.com>
2021-11-29 14:15:06 +0100

    All good now

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ question_bank = []
 
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
 
 quiz = QuizBrain(question_bank)
+print("Hello and welcome to Quiz test. You can end the test at any moment by typing 'end'."
+      "Good luck and have fun!")
 while quiz.still_has_questions():
     quiz.next_question()
+
+print(f"You've completed the quiz. Your final score: {quiz.score}/{len(question_bank)}")
